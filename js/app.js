8ec89af601b198e594f59f6969eb655ddadc414f
William LARDIER <william.lardier96@orange.fr>
2017-09-29 11:35:12 +0200

    Creation of the basic pages

---
+++ js/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const DEBUG = true;
+//Debugging function
+var __debug = function(msg) { if(DEBUG){console.log("%c[DEBUG]%c :"+" %c "+msg+" ", "color:#CAE1E8;background:#526970;", "background:transparent;", "color:#fff;background:#2F3C40;");} }
+
+
+var APP_CANVAS = {
+  init: function() {
+    _debug("Creating the canvas");
+  };
+
+};
