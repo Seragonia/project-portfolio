3ff19b9a9877e432dc2a84f8291471131af29cac
Owana <ana.reze@hotmail.fr>
2017-10-04 14:05:56 +0200

    dev canvas

--- js/app.js
+++ js/app.js
@@ -6,8 +6,24 @@ var __debug = function(msg) { if(DEBUG){console.log("%c[DEBUG]%c :"+" %c "+msg+"
 
 
 var APP_CANVAS = {
+	canvas: null;
+	ctx: null;
+
   init: function() {
     _debug("Creating the canvas");
+    this.canvas = document.getElementById('test')
+    ctx = canvas.getContext('2d');
+  
+
   };
+
+  draw: function() {
+
+  	ctx.fillStyle = "rgb(200, 10, 10)";
+    ctx.fillRect (10, 10, 55, 60);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect (30, 30, 55, 50);
+  }
 
 };
