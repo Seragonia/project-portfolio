73689e938f748344379aefeef4839cffaf2e17f6
William LARDIER <william.lardier96@orange.fr>
2017-10-04 13:55:02 +0200

    testing the canvas

--- js/app.js
+++ js/app.js
@@ -6,8 +6,23 @@ var __debug = function(msg) { if(DEBUG){console.log("%c[DEBUG]%c :"+" %c "+msg+"
 
 
 var APP_CANVAS = {
-  init: function() {
-    _debug("Creating the canvas");
-  };
+  canvas: null,
+  ctx: null,
 
+  init: function() {
+    __debug("Creating the canvas");
+    this.canvas = document.getElementById("header-canvas");
+    if(this.canvas.getContext) {
+      this.ctx = this.canvas.getContext("2d");
+      this.ctx.fillStyle = "rgb(200,0,0)";
+      this.ctx.fillRect (10, 10, 55, 50);
+
+      this.ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+      this.ctx.fillRect (30, 30, 55, 50);
+    }
+  },
+
+  draw: function() {
+
+  }
 };
